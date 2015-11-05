92486e09ac80e8c0b22238cb8e0aa3bc4e4b1b55
zsummers <zsummers@L11702042765.wctc.msft>
2015-11-05 16:50:23 -0600

    used EmployeeBySsn alternate sorter

--- src/lab/class21/Four.java
+++ src/lab/class21/Four.java
@@ -1,8 +1,11 @@
 package lab.class21;
 
 import common.Employee;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -20,15 +23,16 @@ public class Four {
         Employee e3 = new Employee("Hayes","Dinah","345-33-5083");
         Employee e4 = new Employee("Meyer","Oliver","345-33-5083");
         
-        Set<Employee> emps = new TreeSet<>();
-        emps.add(e1);
-        emps.add(e4);
-        emps.add(e2);
-        emps.add(e3);
+        Set<Employee> empsBySsn = new TreeSet<>(new EmployeeBySsn());
+        empsBySsn.add(e1);
+        empsBySsn.add(e4);
+        empsBySsn.add(e2);
+        empsBySsn.add(e3);
         
-        
-        for(Employee e:emps){
-            System.out.println(e);
+        //List<Employee> list = new ArrayList(empsBySsn);
+        //Collections.sort(list, new EmployeeBySsn());
+        for(Employee e:empsBySsn){
+            System.out.println(e.getSsn());
         }//sorted alphabetically
     }
     
