008a1fa2d60d0f1dda23a04a9d6f1defc6f4b98b
zsummers <zsummers@L11702042765.wctc.msft>
2015-11-05 16:35:34 -0600

    corrected keys on Three

--- src/lab/class21/Three.java
+++ src/lab/class21/Three.java
@@ -14,20 +14,21 @@ import java.util.TreeMap;
 public class Three {
     
     public static void main(String[] args) {
-        Employee e1 = new Employee("Abbot","Ronald","234-22-1827");
-        Employee e2 = new Employee("Smith","Ivan","123-11-7345");
+        Employee e1 = new Employee("Smith","Ivan","123-11-7345");
+        Employee e2 = new Employee("Abbot","Ronald","234-22-1827");
         Employee e3 = new Employee("Hayes","Dinah","345-33-5083");
         Employee e4 = new Employee("Meyer","Oliver","345-33-5083");
         
         Map<String,Employee> emps = new TreeMap<>();
         emps.put("Smith",e1);
-        emps.put("Abbot",e4);
+        emps.put("Meyer",e4);
+        emps.put("Abbot",e2);
         emps.put("Hayes",e3);
-        emps.put("Meyer",e2);
+        
         Collection<Employee> e_vals = emps.values();
         for(Employee e:e_vals){
             System.out.println(e);
-        }//sorted by descending numeric order of SSN first
+        }//sorted alphabetically by lastName first
         Set<String> e_keys = emps.keySet();
         for(String k:e_keys){
             System.out.println(k);
